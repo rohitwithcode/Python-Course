b5f07b293bc1b0d5b3231b746cbe63fec5598a56
rohitwithcode <radharani7253@gmail.com>
2024-01-17 20:43:33 +0530

    Operators added

---
+++ Chapter_1_Variables_and_DataTypes/Operators.py
@@ -0,0 +1,7 @@
+print(3+3)  # Addition Operator
+print(4-5)  # Subtraction Operator
+print(5*34) # Multiplying Operator
+print(5/2)  # Devision Operator
+print(4%2)  # Module Operator
+print(4//3) # Floor Operator
+print(5**3) # Exponetial Operator
