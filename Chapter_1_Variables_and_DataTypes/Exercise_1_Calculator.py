e4b92c168272abf1016eaa23a543e8007ae97e86
rohitwithcode <radharani7253@gmail.com>
2024-01-17 20:52:03 +0530

    Exercise added

---
+++ Chapter_1_Variables_and_DataTypes/Exercise_1_Calculator.py
@@ -0,0 +1,9 @@
+a=4
+b=3
+
+print("The addition of the ",a,"+",b," is ",(a+b))
+print("The Multiplication of the ",a,"*",b," is ",(a*b))
+print("The Subtraction of the ",a,"-",b," is ",(a-b))
+print("The Devidation of the ",a,"/",b," is ",(a/b))
+print("The Exponential of the ",a,"**",b," is ",(a**b))
+print("The Module of the ",a,"%",b," is ",(a%b))
