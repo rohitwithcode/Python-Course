ed3feb28903b3215cea98856d73600ba8cebdf20
rohitwithcode <radharani7253@gmail.com>
2024-01-17 20:20:29 +0530

    files added

---
+++ Chapter_1_Variables_DataTypes_and Operators/Variables_and_DataTypes.py
@@ -0,0 +1,31 @@
+a=123243439847394837
+print(a)
+a1=1.2
+print(a1)
+a2=complex(3,4)
+print(a2)
+a3=46676.909090909
+print(a3)
+b=True
+print(b)
+c="Rohit"
+print(c)
+d=None
+print(d)
+
+print("The Type of a is : ",type(a))
+print("The Type of a1 is : ",type(a1))
+print("The Type of a2 is : ",type(a2))
+print("The Type of a3 is : ",type(a3))
+print("The Type of b is : ",type(b))
+print("The Type of c is : ",type(c))
+print("The Type of d is : ",type(d))
+
+list1=[2,3.4,[-34,5],["Coder,Coding"]]
+print(list1)
+
+tuple1=(("Code","Coder"),("School,Coaching"))
+print(tuple1)
+
+dict1={"name":"Rohit","age":19,"canPlay":True}
+print(dict1)
