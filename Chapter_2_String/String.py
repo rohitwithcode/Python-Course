9a42b48d79424bd335ec39cbef934de10bc3f1df
rohitwithcode <radharani7253@gmail.com>
2024-01-18 20:30:43 +0530

    String file added

---
+++ Chapter_2_String/String.py
@@ -0,0 +1,24 @@
+str1="coding"
+str2="Coder"
+
+print("Hi ",str2)
+
+enginner='Coding is " Best carrer option'
+print(enginner)
+
+# Muliple line string we make from Triple single quotes('''''') or Triple doule quotes ("""""")
+
+# mulitple_line='''My name is Rohit Yadav.
+# I am a boy.
+# my hobby is coding.
+# this is my goal.
+# Thank you
+# '''
+# print(mulitple_line)
+
+print(str1[0])
+print("I am print characters using for loop....")
+
+for values in str1:
+    print(values)
+    
