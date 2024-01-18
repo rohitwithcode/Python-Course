669db930b4d1b0f88b5200e0c56a12626a8565e9
rohitwithcode <radharani7253@gmail.com>
2024-01-18 20:54:10 +0530

    String Slicing File added

---
+++ Chapter_2_String/String_Slicing_and_Operations.py
@@ -0,0 +1,16 @@
+names="Rohit,Mohit"
+print(len(names))
+print(names[0:5])
+
+game="cricket"
+gameLength=len(game)
+print("Cricket is the ",gameLength," word .")
+
+# print(game[0:6])
+# print(game[:5])
+# print(game[1:5])
+# print(game[:])
+# print(game[0:-5])
+# print(game[0:len(game)-5])
+print(game[-1:-5])
+print(game[-5:-1])
