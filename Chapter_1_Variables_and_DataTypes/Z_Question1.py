cf26257eb15e6bec2ef66448d51f88c23faa646b
rohitwithcode <radharani7253@gmail.com>
2024-01-18 20:06:49 +0530

    Question added

---
+++ Chapter_1_Variables_and_DataTypes/Z_Question1.py
@@ -0,0 +1,16 @@
+a=input("Enter first number : ")
+b=input("Enter Second Number :")
+print(a + b)
+
+a=input("Enter first number : ")
+b=input("Enter Second Number :")
+print(int(a) + int(b))
+
+a=input("Enter first number as float : ")
+b=input("Enter Second Number as float :")
+print(a + b)
+
+a=input("Enter first number as float : ")
+b=input("Enter Second Number as float :")
+print(float(a) + float(b))
+
