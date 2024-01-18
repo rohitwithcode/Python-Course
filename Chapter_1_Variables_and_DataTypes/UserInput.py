ba62002e559cc9a9a2a2e21947f23049c7abbada
rohitwithcode <radharani7253@gmail.com>
2024-01-18 19:58:32 +0530

    User Input File added

---
+++ Chapter_1_Variables_and_DataTypes/UserInput.py
@@ -0,0 +1,11 @@
+# a=input()
+# print(a)
+
+# b=input("Enter your good name please : ")
+# print(b)
+
+x=input("Enter first number : ")
+y=input("Enter your second number : ")
+# print(x+y)
+print(int(x) + int(y))
+
